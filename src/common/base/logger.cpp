37f283aed0b4c0614ebd12418b22c1ede4b9dcf2
oy <ouyangjian@c5game.com>
2025-12-07 12:25:38 +0800

    fix: logger重复注册

--- src/common/base/logger.cpp
+++ src/common/base/logger.cpp
@@ -24,15 +24,16 @@ std::shared_ptr<spdlog::logger> Logger::getLogger(const std::string &logger_name
     auto max_size = 1048576 * 10;
     auto max_files = 20;
 
+    auto file_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>("log/" + logger_name + ".log", max_size, max_files);
+
     if (to_console)
     {
         auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-        auto file_sink = std::make_shared<spdlog::sinks::rotating_file_sink_mt>("log/" + logger_name + ".log", max_size, max_files);
         logger = std::make_shared<spdlog::logger>(logger_name, spdlog::sinks_init_list {file_sink, console_sink});
     }
     else
     {
-        logger = spdlog::rotating_logger_mt(logger_name, "log/" + logger_name + ".log", max_size, max_files);
+        logger = std::make_shared<spdlog::logger>(logger_name, spdlog::sinks_init_list {file_sink});
     }
 
     logger->set_pattern("[%Y-%m-%d %H:%M:%S %z][%t][%^%l%$][%s:%#:%!]: %v"); // (%@)
